b99d6be57794aee7b583437cce3843839aa27181
Christian Fleschhut <weltrekordhalter@gmail.com>
2014-02-02 19:19:27 +0100

    basic album model + view

--- public/js/Tunes.js
+++ public/js/Tunes.js
@@ -1,3 +1,18 @@
 (function($) {
 
+  window.Album = Backbone.Model.extend({});
+
+  window.AlbumView = Backbone.View.extend({
+    tagName: 'li',
+    className: 'album',
+    initialize: function() {
+      this.template = _.template($('#album-template').html());
+    },
+    render: function() {
+      var renderedContent = this.template(this.model.toJSON());
+      $(this.el).html(renderedContent);
+      return this;
+    }
+  });
+
 })(jQuery);
